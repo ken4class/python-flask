5c301edc1497bbaf5011f91379b6eac0a6f76f12
kendevops4 <ken4@k-kloud.net>
2023-07-30 10:53:16 -0400

    Added project to repo

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        try:
+            num1 = float(request.form['num1'])
+            num2 = float(request.form['num2'])
+            product = num1 * num2
+            return render_template('answer.html', product=product)
+        except ValueError:
+            return "Please enter valid numbers."
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9000)
